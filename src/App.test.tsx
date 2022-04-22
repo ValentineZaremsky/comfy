2b5cc6bc3d7f53d73375bf15d57ee198fa4deeb7
Valentine Zaremsky <zaremsky@gmail.com>
2022-04-23 00:08:54 +0300

    Fix App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { CategoriesProps, ProductProps } from '.';
 import App from './App';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let categories: Array<CategoriesProps> = [];
+let products: Array<ProductProps> = [];
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <App categories={categories} products={products} />
+    </BrowserRouter>,
+    div,
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
